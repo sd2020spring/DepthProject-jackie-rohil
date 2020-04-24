5392790f44689c6b9cd953ad4ed552314efe4a26
Rohil <rohil@rohil-Latitude-5401>
2020-04-24 11:58:00 -0400

    adaptive thresholding

--- src/OpenCV.py
+++ src/OpenCV.py
@@ -6,9 +6,26 @@ cap = cv2.VideoCapture(0)
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    threshframe = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+    contours,h = cv2.findContours(threshframe,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
     # Display the resulting frame
-    cv2.imshow('frame', frame)
+    cv2.imshow('threshframe', threshframe)
+
+    for cnt in contours:
+        approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+        print(len(approx))
+        if len(approx)==5:
+            print("pentagon")
+            cv2.drawContours(threshframe,[cnt],0,255,-1)
+        elif len(approx)==3:
+            print("triangle")
+            cv2.drawContours(threshframe,[cnt],0,(0,255,0),-1)
+        elif len(approx)==4:
+            print("rectangle")
+            cv2.drawContours(threshframe,[cnt],0,(0,0,255),-1)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
