d39a0c6c23d390f8b3e4e8b3e907523caebf5c85
Jackie <dolphin.pool01@gmail.com>
2020-05-05 14:05:14 -0400

    Detects rectangle and prints circle on the screen in real time

--- rohil_example.py
+++ rohil_example.py
@@ -4,6 +4,10 @@ sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages') # in order to im
 import cv2
 sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages') # append back in order to import rospy
 
+# cap = cv2.VideoCapture(0)
+# img = cv2.imread(File_path, cv2.IMREAD_GRAYSCALE)
+
+
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -11,23 +15,23 @@ while True:
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     threshframe = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
-    contours,h = cv2.findContours(threshframe,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    contours,h = cv2.findContours(threshframe,4,2)
 
     # Display the resulting frame
     cv2.imshow('threshframe', threshframe)
 
     for cnt in contours:
         approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-        print(len(approx))
+        print (len(approx))
         if len(approx)==5:
-            print("pentagon")
-            cv2.drawContours(threshframe,[cnt],0,255,-1)
+            print ("pentagon")
+            cv2.drawContours(img,[cnt],0,255,-1)
         elif len(approx)==3:
-            print("triangle")
-            cv2.drawContours(threshframe,[cnt],0,(0,255,0),-1)
+            print ("triangle")
+            cv2.drawContours(img,[cnt],0,(0,255,0),-1)
         elif len(approx)==4:
-            print("rectangle")
-            cv2.drawContours(threshframe,[cnt],0,(0,0,255),-1)
+            print ("rectangle")
+            cv2.drawContours(img,[cnt],0,(0,0,255),-1)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
