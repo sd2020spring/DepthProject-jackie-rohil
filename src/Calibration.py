f6388d7bfcee99ab81bc1024c06a27cff320c20e
jackiezeng01 <dolphin.pool01@gmail.com>
2020-05-08 01:40:14 -0400

    commented out ROS stuff

--- src/Calibration.py
+++ src/Calibration.py
@@ -1,8 +1,8 @@
 import pygame
 import sys
-sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages') # in order to import cv2 under python3
+# sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages') # in order to import cv2 under python3
 import cv2
-sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages') # append back in order to import rospy
+# sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages') # append back in order to import rospy
 import numpy as np
 
 # OpenCV Setup
